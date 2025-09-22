0c4d207d1cf5ae50d11a130689242be3a88676ad
Nityagopaljena98 <nityagjena@hotmail.com>
2025-09-22 11:49:14 +0530

    add print first three palindrome number

--- New JavaScript Class/Programs/Looping/For Loop/print-first-three-palindrome-no.js
+++ New JavaScript Class/Programs/Looping/For Loop/print-first-three-palindrome-no.js
@@ -1,6 +1,7 @@
 
-// Write a program to print palindrome number from 10 to 100 
+// Write a program to print first three palindrome number 
 
+let count = 0;
 for (let i = 10; i <= 100; i++) {
     let num = i;
     let rev = 0;
@@ -9,8 +10,11 @@ for (let i = 10; i <= 100; i++) {
         rev = rev * 10 + rem;
         num = Math.trunc(num / 10)
     }
-
     if (rev == i) {
+        count++;
         console.log(i);
+        if (count == 3)
+            break;
+
     }
 }
