07cc062bb66658aeb67651c718a07cc7d67c8c04
Nityagopaljena98 <nityagjena@hotmail.com>
2025-09-25 12:16:18 +0530

    add pattern3 pattern program file

---
+++ New JavaScript Class/Programs/Patterns/pattern3.js
@@ -0,0 +1,25 @@
+
+
+// Print below pattern
+
+/*
+  * * * * * 
+  * * * * * 
+  * * * * * 
+  * * * * * 
+  * * * * * 
+*/
+
+let n = 5;
+let res = "";
+
+
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+        res += "*" + " "
+    }
+    res += "\n";
+
+}
+
+console.log(res);
