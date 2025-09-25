6cf9cf268f32efaebbc9972519ae545cabc101a8
Nityagopaljena98 <nityagjena@hotmail.com>
2025-09-25 12:01:57 +0530

    add pattern1 program file

--- New JavaScript Class/Programs/Looping/For Loop/print-1-100-palindrome-number.js
+++ New JavaScript Class/Programs/Looping/For Loop/print-1-100-palindrome-number.js
@@ -14,3 +14,4 @@ for (let i = 10; i <= 100; i++) {
         console.log(i);
     }
 }
+
---
+++ New JavaScript Class/Programs/Patterns/pattern1.js
@@ -0,0 +1,42 @@
+
+// Print 1 to 5 five times
+
+/* Output :-
+1
+2
+3
+4
+5
+1
+2
+3
+4
+5
+1
+2
+3
+4
+5
+1
+2
+2
+2
+3
+4
+5
+1
+2
+3
+4
+5
+
+*/
+
+
+let n = 5;
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+        console.log(j);
+
+    }
+}
