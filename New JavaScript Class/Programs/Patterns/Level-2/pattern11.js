1bd9b6d4dbe2d0b90d88210b20fcf7c0c603aad6
Nityagopaljena98 <nityagjena@hotmail.com>
2025-09-29 12:15:29 +0530

    added pattern11 program file

--- New JavaScript Class/Programs/Patterns/Level-2/pattern11.js
+++ New JavaScript Class/Programs/Patterns/Level-2/pattern11.js
@@ -1,3 +1,27 @@
 
-// Print  the below pattern
+/* Print  the below pattern
 
+    *
+   **
+  ***
+ ****
+*****
+
+*/
+
+
+let n = 5;
+let res = "";
+for (let i = 1; i <= 5; i++) {
+    for (let j = 1; j <= n - i; j++) {
+        res += " "
+    }
+
+    for (let j = 1; j <= i; j++) {
+        res += "*"
+    }
+
+    res += '\n'
+}
+
+console.log(res);
