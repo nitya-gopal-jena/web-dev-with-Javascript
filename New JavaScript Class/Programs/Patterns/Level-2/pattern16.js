05e2ca88bef613d9f051beb2c442568a37a9c837
Nityagopaljena98 <nityagjena@hotmail.com>
2025-10-06 11:46:26 +0530

    add pattern16 program file

---
+++ New JavaScript Class/Programs/Patterns/Level-2/pattern16.js
@@ -0,0 +1,31 @@
+
+/*
+ Print the below pattern 
+
+        * 
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * *
+
+*/
+
+let n = 5;
+let res = '';
+let space = n - 1;
+let stars = 1;
+
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= space; j++) {
+        res += "  ";
+    }
+    for (let j = 1; j <= stars; j++) {
+        res += "* ";
+    }
+
+    res += "\n"
+    space--;
+    stars += 2;
+}
+
+console.log(res);
