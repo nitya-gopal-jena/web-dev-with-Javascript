ac273fbc3a958173e84eee9aaadbcd588cf78a9c
Nityagopaljena98 <nityagjena@hotmail.com>
2025-10-09 17:33:12 +0530

    add pattern19 file

---
+++ New JavaScript Class/Programs/Patterns/Level-2/pattern19.js
@@ -0,0 +1,38 @@
+
+
+/*
+  Print the below pattern
+
+* * * * * * * * * 
+  *           *
+    *       *
+      *   *
+        *
+
+*/
+
+
+
+let n = 5;
+let res = '';
+let space = 0;
+let stars = 9;
+
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= space; j++) {
+        res += "  ";
+    }
+    for (let j = 1; j <= stars; j++) {
+        if (i == 1 || j == 1 || j == stars) {
+            res += "* ";
+        } else {
+            res += "  "
+        }
+    }
+
+    res += "\n"
+    space++;
+    stars -= 2;
+}
+
+console.log(res);
