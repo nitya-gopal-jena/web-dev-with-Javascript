c83b58221c86dbe18c31910c7142a3854cd7ec14
Nityagopaljena98 <nityagjena@hotmail.com>
2025-10-09 17:33:34 +0530

    add pattern20 file

---
+++ New JavaScript Class/Programs/Patterns/Level-2/pattern20.js
@@ -0,0 +1,47 @@
+
+
+/*
+
+ Print the below pattern 
+
+        * 
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * *
+  * * * * * * *
+    * * * * *
+      * * *
+        *
+
+
+*/
+
+
+
+let n = 9;
+let res = '';
+let space = 4;
+let stars = 1;
+
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= space; j++) {
+        res += "  ";
+    }
+    for (let j = 1; j <= stars; j++) {
+        res += "* ";
+    }
+
+    res += "\n"
+    if (i <= Math.trunc(n / 2)) {
+        space--;
+        stars += 2;
+    } else {
+        stars -= 2;
+        space++
+    }
+
+
+}
+
+console.log(res);
